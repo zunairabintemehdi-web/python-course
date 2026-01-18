36356c9ee744fd1e74c7fa41f413721eb9ffe3cc
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-01-18 17:23:17 +0600

    lesson 1 activity 1 done

--- lesson 1 activity 1/main.py
+++ lesson 1 activity 1/main.py
@@ -3,3 +3,6 @@ print("my name is zunaira")
 print("I love playing badminton")
 #ADDITION
 print("the result of 27+21",27+21)
+print("what is your name?")
+name=input()
+print(f"nice to meet you,{name}")
---
+++ lesson 2 activity 1/main.py
@@ -0,0 +1,15 @@
+name="zoon"
+age=11
+is_student=True 
+weight=33.5
+print("name",name)
+print("data type of name is",type(name))
+print("age",age)
+print("data type of age is",type(age))
+print("is_student",is_student)
+print("data type of is_student is",type(is_student))
+print("weight",weight)
+print("data type of weight is",type(weight))
+
+age=str(age)
+print("new data type of age is",type(age))
