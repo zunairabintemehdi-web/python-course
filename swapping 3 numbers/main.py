ef29d54e8d2fd5f58ea81ef83970a774f2e7cbd9
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-01-18 20:15:06 +0600

    lesson 2 activity 3 done

---
+++ swapping 3 numbers/main.py
@@ -0,0 +1,13 @@
+x=input("enter value of x:")
+y=input("enter value of y:")
+z=input("enter value of z:")
+
+temp=z
+temp=y
+y=x
+x=temp
+z=y
+temp=y
+print("value of x after swapping",x)
+print("value of y after swapping",y)
+print("value of z after swapping",z)
