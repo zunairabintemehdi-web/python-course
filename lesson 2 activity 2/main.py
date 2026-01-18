ae7607b89b214eb08fd44ca8f7e84543c5dcc91a
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-01-18 17:49:26 +0600

    lesson 2 activity 2 done

---
+++ lesson 2 activity 2/main.py
@@ -0,0 +1,15 @@
+num1=27
+num2=40
+print("addition",num1+num2)
+print("substraction",num2-num1)
+print("multipication",num1*num2)
+print("division",num2/num1)
+print("floor",num2//num1)
+print("square",num1**8)
+print("square root",num1**0.5)
+print("modules",num1%num2)
+
+print("equal",num2==num1)
+print("greater than",num1>num2)
+print("less than",num1<num2)
+print("not equal to",num2!=num1)
