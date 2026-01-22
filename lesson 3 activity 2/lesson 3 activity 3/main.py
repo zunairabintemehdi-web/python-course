99253bd8e2d3c3421154aad5a8664d78afdd4505
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-01-22 10:22:45 +0600

    lesson 3 activity 3 done

---
+++ lesson 3 activity 2/lesson 3 activity 3/main.py
@@ -0,0 +1,12 @@
+num=int (input("enter number to check:"))
+if num>67:
+    print("number is grater than 67")
+    if num%2==0:
+        print("and it is also even")
+    else:
+        print("and it is odd")
+
+else:
+        print("no. is less than 67")
+
+       
