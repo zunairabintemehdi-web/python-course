32248a5f2d16ddf40ffba4ed08c0702697108828
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-02-18 18:09:33 +0600

    lesson 8 activity 4 done

---
+++ lesson 8 activity 4/main.py
@@ -0,0 +1,8 @@
+setx = {"green", "blue"}
+sety = {"blue", "yellow"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
