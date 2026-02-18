dbc90a86f5b287cd0dbe2d12fe9089dd18edc853
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-02-18 17:48:39 +0600

    done

---
+++ number guessing game/main.py
@@ -0,0 +1,26 @@
+int(input("enter"))
+
+if 0:
+    print("zero")
+
+if 1:
+    print("one")
+
+if 2:
+    print("two")
+if 3:
+    print("three")
+if 4:
+    print("four")
+elif 5:
+   print("five")
+if 6:
+    print("six")
+if 7:
+    print("seven")
+if 8:
+    print("eight")
+if 9:
+    print("nine")
+if 10:
+    print("ten")
