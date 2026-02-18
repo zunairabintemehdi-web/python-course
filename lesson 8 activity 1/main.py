1fecb933adfb2d45390cc89fc094830cd214c774
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-02-18 17:24:40 +0600

    lesson 8 activity 1 done

---
+++ lesson 8 activity 1/main.py
@@ -0,0 +1,21 @@
+my_tuple=()
+print(my_tuple)
+my_tuple =(1,2,3)
+print(my_tuple)
+my_tuple = (1, "hello", 6.7)
+print=(my_tuple)
+my_tuple=("mouse",[1,3,5],(1,2,4))
+print=(my_tuple)
+
+my_tuple=('p','o','o','k','i','e')
+print(my_tuple[1])
+print(my_tuple[4])
+
+n_tuple = ("mouse", [2,3,4],(2,3,5))
+print(n_tuple[0][3])
+print(n_tuple[1][1])
+
+print("sliced:", my_tuple[1:5])
+
+for letter in (my_tuple):
+    print("hi you are a",letter)
