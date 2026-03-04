20c5bd2dc533fe029f4278412d9d628c30d4a8e7
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-03-04 14:07:30 +0600

    done

--- lesson 10 activity 2/main.py
+++ lesson 10 activity 2/main.py
@@ -18,3 +18,54 @@ class Library :
     def addBook(self, book):
         self.booklist.append(book)
         print("book has been addedto the book list")
+    def returnBook(self, book):
+        self.lendDict.pop(book)
+
+if __name__ == '__main__':
+    
+    books = Library(['Python', 'Rich Dad Poor Dad', 'Harry Potter', 'C++ Basics', 'Algorithms by CLRS'], "Let's Upskill")
+
+    while(True):
+        print(f"Welcome to the {books.name} library. Enter your choice to continue")
+        print("1. Display Books")
+        print("2. Lend a Book")
+        print("3. Add a Book")
+        print("4. Return a Book")
+        user_choice = input()
+        if user_choice not in ['1','2','3','4']:
+            print("Please enter a valid option")
+            continue
+
+        else:
+            user_choice = int(user_choice)
+
+
+        if user_choice == 1:
+            books.displayBooks()
+
+        elif user_choice == 2:
+            book = input("Enter the name of the book you want to lend:")
+            user = input("Enter your name")
+            books.lendBook(user, book)
+
+        elif user_choice == 3:
+            book = input("Enter the name of the book you want to add:")
+            books.addBook(book)
+
+        elif user_choice == 4:
+            book = input("Enter the name of the book you want to return:")
+            books.returnBook(book)
+
+        else:
+            print("Not a valid option")
+
+
+        print("Press q to quit and c to continue")
+        user_choice2 = ""
+        while(user_choice2!="c" and user_choice2!="q"):
+            user_choice2 = input()
+            if user_choice2 == "q":
+                exit()
+
+            elif user_choice2 == "c":
+                continue
---
+++ lesson 11 activity 1/main.py
@@ -0,0 +1,23 @@
+class Person(object ):
+
+
+    def __init__(self, name, idnumber):
+        self.name= name
+        self.idnumber= idnumber
+    def display(self):
+        print(self.name)
+        print(self.idnumber)
+
+class Employee (Person):
+    def __init__(self, name,idnumber, salary, post, ):
+        self.salary= salary
+        self.post = post
+
+        Person.__init__(self, name, idnumber)
+
+a = Employee('Penguin', 23456676 , 560000, "Intern")
+
+a.display()
+
+
+
