5eacef4a1d09eb040a0a40b85c1ba7cc69ed55b7
zunairabintemehdi-web <zunairabintemehdi@gmail.com>
2026-03-16 00:32:41 +0600

    lesson 12 activity 2

--- lesson 12 activity 2/main.py
+++ lesson 12 activity 2/main.py
@@ -1,23 +1,15 @@
-class Computer:
+class Computer :
     def __init__(self):
-        self.__maxprice= 1000
-
+        self.__maxprice = 1000
     def sell(self):
-        print ("selling price : {}".format(self.maxprice))
+        print("Selling Price:{}">format(self.__maxprice))
     def setMaxPrice(self,price):
         self.__maxprice = price
-
-
-
 c = Computer()
+c.sell
+
+c.__maxprice = 1500
 c.sell()
 
-
-
-c.__maxprice= 1200
-c.sell()
-
-
-
-c.setMaxPrice(1200)
+c.setMaxPrice(1500)
 c.sell()
